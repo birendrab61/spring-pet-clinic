a4ef928610b77c9872224310d27e5f4d30a70271
Bimal Pathak <bimalpathak20@gmail.com>
2020-01-30 15:46:42 -0600

    Refactoring to multimodule maven project. closes #1

--- pet-clinic-data/src/main/java/com/pathakbimal/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/pathakbimal/springpetclinic/model/Pet.java
@@ -1,14 +1,36 @@
 package com.pathakbimal.springpetclinic.model;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 import java.time.LocalDate;
 
-@Setter
-@Getter
+
 public class Pet {
     private PetType petType;
     private Owner owner;
     private LocalDate birthdate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
 }
